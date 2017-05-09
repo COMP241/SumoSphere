88564a5e6cb880ee0b848b5db61d69a7f5419afd
jacobcheatley <jacobcheatley@gmail.com>
2017-05-09 21:05:24 +1200

    Initial commit

---
+++ Assets/Scripts/GetRequestExample.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetRequestExample : MonoBehaviour
+{
+    private ImageMap map;
+    
+    IEnumerator Start()
+    {
+        int id = 0; // Change this for user inputted id
+        using (UnityWebRequest www = UnityWebRequest.Get("http://papermap.tk/api/map/" + id))
+        {
+            yield return www.Send();
+
+            if (!www.isError)
+            {
+                map = ImageMap.FromJson(www.downloadHandler.text);
+                DoStuff();
+            }
+            else
+            {
+                Debug.Log(www.error);
+            }
+        }
+    }
+
+    void DoStuff()
+    {
+        // Now do stuff with map, for example, turn this class into a "LevelGenerator" MonoBehaviour and start creating objects in the level based off the map data
+        Debug.Log(map.ToString());
+    }
+}
---
+++ Assets/Scripts/ImageMap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class ImageMap
+{
+    public int Id;
+    public float Ratio;
+    public Line[] Lines;
+
+    public static ImageMap FromJson(string json)
+    {
+        return JsonUtility.FromJson<ImageMap>(json);
+    }
+
+    public override string ToString()
+    {
+        return string.Format("MAP\nid:{0}\nratio:{1}\nLines:\n\t{2}", Id, Ratio, string.Join("\n\t", Lines.Select(l => l.ToString()).ToArray()));
+    }
+}
+
+[Serializable]
+public enum MapColor
+{
+    Black = 0,
+    Red = 1,
+    Green = 2,
+    Blue = 3,
+    Cyan = 4,
+    Magenta = 5,
+    Yellow = 6
+}
+
+[Serializable]
+public class Line
+{
+    public MapColor Color;
+    public bool Loop;
+    public Point[] Points;
+
+    public override string ToString()
+    {
+        return string.Format("{0} {1} [{2}]", Color, Loop ? "Looping" : "Open", string.Join(",", Points.Select(p => p.ToString()).ToArray()));
+    }
+}
+
+[Serializable]
+public class Point
+{
+    public float X;
+    public float Y;
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1})", X, Y);
+    }
+}
