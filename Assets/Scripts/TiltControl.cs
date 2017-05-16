beecba0a8111b1602edec82dad8dd448c4b01141
jacobcheatley <jacobcheatley@gmail.com>
2017-05-16 22:39:42 +1200

    Removed unused (and broken) code.

--- Assets/Scripts/TiltControl.cs
+++ Assets/Scripts/TiltControl.cs
@@ -20,11 +20,6 @@ public class TiltControl : MonoBehaviour
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * 30f);
     }
 
-    private static Quaternion GyroToUnity(Quaternion q)
-    {
-        return new Quaternion(q.x, q.y, -q.z, -q.w);
-    }
-
     private static Quaternion ChangeAttitude(Quaternion q)
     {
         return new Quaternion(q.y, 0.0f, -q.x, 1.0f);
