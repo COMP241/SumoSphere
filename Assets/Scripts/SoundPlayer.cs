551014681116dc0e1c451700e5d2432eda643ee3
jacobcheatley <jacobcheatley@gmail.com>
2017-06-08 22:07:46 +1200

    Added sound player

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    private static SoundPlayer instance;
+
+    [Header("Sources")]
+    [SerializeField] private AudioSource sfxSource;
+
+    private void Start()
+    {
+        if (instance == null)
+            instance = GetComponent<SoundPlayer>();
+        else
+            Destroy(gameObject);
+    }
+
+    public static void PlaySFX(AudioClip clip, float volume)
+    {
+        instance.sfxSource.PlayOneShot(clip, volume);
+    }
+}
